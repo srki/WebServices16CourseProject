516e8a8c87400d1ea4c2e599a061be8fd4ec1728
Dimitrije Jankov <dimitrijejankov@gmail.com>
2016-05-05 00:24:54 +0200

    Register and Logout added

--- WS16_2012/views/login.py
+++ WS16_2012/views/login.py
@@ -1,8 +1,10 @@
 import json
 
 from django.views.generic import View
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class LoginView(View):
@@ -22,6 +24,22 @@ class LoginView(View):
             return JsonResponse({'message': "The provided credentials are not valid!"}, status=400)
 
 
+class LogoutView(View):
+    def post(self, request):
+        logout(request)
+
+
 class RegisterView(View):
     def post(self, request):
-        pass
+        try:
+            credentials = json.loads(request.body)
+            User.objects.get(username=credentials['username'])
+
+            return JsonResponse({'message': 'An user with that username already exists!'}, status=400)
+        except ObjectDoesNotExist:
+            u = User.objects.create_user(username=['username'], password=credentials['password'])
+            u.save()
+
+            return JsonResponse({}, status=200)
+        except ValueError:
+            return JsonResponse({'message': 'Invalid JSON!'}, status=400)
--- git/urls.py
+++ git/urls.py
@@ -17,12 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from WS16_2012.views.login import LoginView, RegisterView
+from WS16_2012.views.login import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/login', LoginView.as_view()),
     url(r'^api/register', RegisterView.as_view()),
+    url(r'^api/logout', LogoutView.as_view()),
     url(r'^', TemplateView.as_view(template_name="index.html"))
 ]
 
