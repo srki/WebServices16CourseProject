e5d2d358c41f8364e7671b91da20362a725749b6
Dimitrije Jankov <dimitrijejankov@gmail.com>
2016-05-05 20:06:43 +0200

    Refactored some code

--- WS16_2012/views/project.py
+++ WS16_2012/views/project.py
@@ -8,7 +8,6 @@ from django.utils.decorators import method_decorator
 
 from WS16_2012.views.views import RestView, View
 from WS16_2012.models import Project
-from django.contrib.auth.models import User
 
 from django.core.paginator import Paginator
 
@@ -70,77 +69,3 @@ class ProjectView(View):
             return JsonResponse({'message': 'Invalid JSON!'}, status=400)
         except Exception:
             return JsonResponse({'message': 'Bad request!'}, status=400)
-
-
-class AddParticipantsView(View):
-    @method_decorator(permission_required(perm='auth.admin', raise_exception=True))
-    def post(self, request, identifier):
-        project = Project.objects.get(id=int(identifier))
-
-        if not project:
-            return JsonResponse({'message': 'Not found'}, status=404)
-
-        try:
-            values = json.loads(request.body)
-
-            if 'userId' not in values:
-                return JsonResponse({'message': 'Bad request!'}, status=400)
-
-            u = User.objects.get(id=values['userId'])
-            project.participants.add(u)
-            project.save()
-
-            return JsonResponse({}, status=200)
-        except ValueError:
-            return JsonResponse({'message': 'Invalid JSON!'}, status=400)
-        except Exception as e:
-            return JsonResponse({'message': 'Bad request!'}, status=400)
-
-
-class RemoveParticipantsView(View):
-    @method_decorator(permission_required(perm='auth.admin', raise_exception=True))
-    def post(self, request, identifier):
-        project = Project.objects.get(id=int(identifier))
-
-        if not project:
-            return JsonResponse({'message': 'Not found'}, status=404)
-
-        try:
-            values = json.loads(request.body)
-
-            if 'userId' not in values:
-                return JsonResponse({'message': 'Bad request!'}, status=400)
-
-            u = project.participants.get(id=values['userId'])
-
-            project.participants.remove(u)
-            project.save()
-
-            return JsonResponse({}, status=200)
-        except ValueError:
-            return JsonResponse({'message': 'Invalid JSON!'}, status=400)
-        except Exception:
-            return JsonResponse({'message': 'Bad request!'}, status=400)
-
-
-class ParticipantsView(View):
-    @method_decorator(permission_required(perm='auth.user', raise_exception=True))
-    def get(self, request, identifier):
-
-        try:
-            participants = Project.objects.get(id=int(identifier)).participants.all()
-            count = participants.count()
-
-            if 'per_page' in request.GET and 'page' in request.GET:
-                per_page = request.GET['per_page']
-                page = request.GET['page']
-
-                paginator = Paginator(participants, per_page)
-                participants = paginator.page(page)
-
-            data = [model_to_dict(instance, exclude=['participants'], fields=['username', 'id']) for instance in
-                    participants]
-            return JsonResponse({'users': data, 'count': count}, status=200)
-
-        except Exception as e:
-            return JsonResponse({'message': 'Bad request'}, status=400)
