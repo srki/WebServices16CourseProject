8848b71788377db85f690cf2e7b906b7dca7a48e
Dimitrije Jankov <dimitrijejankov@gmail.com>
2016-05-06 03:47:33 +0200

    Added the ability to change the comments!

--- WS16_2012/views/comments.py
+++ WS16_2012/views/comments.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
@@ -7,7 +8,7 @@ from django.contrib.auth.decorators import permission_required
 from django.utils.decorators import method_decorator
 
 from WS16_2012.views.views import RestView, View
-from WS16_2012.models import Project
+from WS16_2012.models import Project, Comment
 
 from django.core.paginator import Paginator
 
@@ -31,5 +32,57 @@ class CommentsView(View):
 
             data = [model_to_dict(instance) for instance in comments]
             return JsonResponse({'comments': data, 'count': count}, status=200)
+        except Exception as e:
+            return JsonResponse({'message': 'Bad request'}, status=400)
+
+    @method_decorator(permission_required(perm='auth.user', raise_exception=True))
+    def post(self, request, project_id, task_id):
+
+        try:
+            values = json.loads(request.body)
+
+            project = Project.objects.get(id=project_id)
+            t = project.task_set.get(id=task_id)
+
+            c = Comment(text=values['text'],
+                        date=datetime.now(),
+                        task=t,
+                        created=request.user)
+
+            c.save()
+
+            return JsonResponse({}, status=200)
+        except Exception as e:
+            return JsonResponse({'message': 'Bad request'}, status=400)
+
+
+class CommentView(View):
+    @method_decorator(permission_required(perm='auth.user', raise_exception=True))
+    def delete(self, request, project_id, task_id, comment_id):
+
+        try:
+            project = Project.objects.get(id=project_id)
+            task = project.task_set.get(id=task_id)
+            comment = task.comment_set.get(id=comment_id)
+            comment.delete()
+
+            return JsonResponse({}, status=200)
+        except Exception as e:
+            return JsonResponse({'message': 'Bad request'}, status=400)
+
+    @method_decorator(permission_required(perm='auth.user', raise_exception=True))
+    def put(self, request, project_id, task_id, comment_id):
+
+        try:
+            values = json.loads(request.body)
+
+            project = Project.objects.get(id=project_id)
+            task = project.task_set.get(id=task_id)
+
+            comment = task.comment_set.get(id=comment_id)
+            comment.text = values['text']
+            comment.save()
+
+            return JsonResponse({}, status=200)
         except Exception as e:
             return JsonResponse({'message': 'Bad request'}, status=400)
